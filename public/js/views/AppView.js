40bdea3d79cea5bb0776d201afee0f0e94985ee4
Jack Zhang <15jzhang@gmail.com>
2016-06-06 01:11:30 -0700

    Add basic frame for AppView

---
+++ public/js/views/AppView.js
@@ -0,0 +1,14 @@
+var AppView = Backbone.View.extend({
+  el: 'div.app',
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    this.$el.append([
+      
+    ]);
+    return this;
+  }
+});
