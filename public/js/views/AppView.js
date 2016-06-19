3a52ab0dad9afddacb55da7d1eaae214d618f436
Jack Zhang <15jzhang@gmail.com>
2016-06-19 13:37:17 -0700

    Add WeatherView with AppView logic

--- public/js/views/AppView.js
+++ public/js/views/AppView.js
@@ -2,13 +2,18 @@ var AppView = Backbone.View.extend({
   el: 'div#app',
 
   initialize: function() {
-    this.title = new TitleView();
+    this.titleView = new TitleView();
+    this.weatherView = new WeatherView({
+      collection: this.collection
+    });
+
     this.render();
   },
 
   render: function() {
     this.$el.append([
-      this.title.$el
+      this.titleView.$el,
+      this.weatherView.$el
     ]);
     return this;
   }
---
+++ public/js/views/WeatherView.js
@@ -0,0 +1,28 @@
+var WeatherView = Backbone.View.extend({
+
+  el: 'div.weather-list',
+
+  initialize: function() {
+    this.listenTo(this.collection, 'add', this.render);
+    //this.collection.on('add', this.render, this);
+
+    this.render();
+  },
+
+  render: function() {
+    this.$el.empty();
+
+    this.weatherCardViews = this.collection.models.map(function(model) {
+      return new WeatherCardView({
+        model: model
+      });
+    });
+
+    var $els = this.weatherCardViews.map(function(entry) {
+      return entry.$el;
+    });
+    this.$el.append($els);
+
+    return this;
+  }
+});
