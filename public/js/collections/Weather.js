5a9eb6e123db14fc4cd19afd06a22c0f11893e1d
Jack Zhang <15jzhang@gmail.com>
2016-06-20 15:57:54 -0700

    Remove AppModel, implement AJAX request to Open Weather API

--- public/js/collections/Weather.js
+++ public/js/collections/Weather.js
@@ -2,6 +2,14 @@ var Weather = Backbone.Collection.extend({
   model: WeatherCard,
 
   addWeatherCard: function(latitude, longitude) {
-    console.log(typeof latitude, typeof longitude);
+    latitude = Number(latitude);
+    longitude = Number(longitude);
+
+    $.get(
+      'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +'&APPID=25bea2da2b435f9371fdf055cf71d548',
+      function(data) {
+        console.log(data);
+      }.bind(this)
+    )
   }
 });
